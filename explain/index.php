05a80bd7c3a265193addac1a0c9875f12c3ffb28
nommy <nomuramasam@gmail.com>
2019-03-21 21:20:17 +0900

    説明ページの見た目を整える

---
+++ explain/index.php
@@ -0,0 +1,39 @@
+<?php require_once('../head.php') ?>
+<div class='container'>
+	<h5 class='my-3 text-center'>バイト地図の使い方</h5>
+	
+	<div class='py-4'>
+		<p class='text-center'><a href='https://shotworks.jp/sw/list/a_01/work?sv=' target='_blank' class='btn btn-secondary border'>まずはショットワークスのサイトへ</a></p>
+	</div>
+	
+	<div class='py-4'>
+		<p class='text-center'>ー 条件などを指定 ー</p>
+		<p><img src='img/explain1.png' class='border rounded'></p>
+	</div>
+	
+	<div class='py-4'>
+		<p class='text-center'>ー URLをコピー ー</p>
+		<p><img src='img/explain2.png' class='border rounded'></p>
+	</div>
+	
+	<div class='py-4'>
+		<p class='text-center'>ー 貼付け ー</p>
+		<p><img src='img/explain3.png' class='border rounded'></p>
+	</div>
+	<div class='py-4'>
+	
+		<p class='text-center'>ー こうなったら成功 ー</p>
+		<p><img src='img/explain4.png' class='border rounded'></p>
+	</div>
+	
+	<div class='py-4'>
+		<p class='text-center'>ー マーカークリックで詳細を表示 ー</p>
+		<p><img src='img/explain5.png' class='border rounded'></p>
+	</div>
+</div>
+
+<style>
+	img{max-width:100%;}
+</style>
+
+
