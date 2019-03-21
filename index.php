9d962d90812a3e099035f4c45bb38ecb00be0715
nommy <nomuramasam@gmail.com>
2019-03-21 19:49:37 +0900

    Bootstrap読み込み

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <link rel='icon' href='./favicon.ico'> <!-- ファビコン -->
 <title>バイト探し地図</title>
 
+<!--Bootstrap４に必要なCSSとJavaScriptを読み込み-->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 <form action='./' method='post'>
 	<b>バイト先を地図で探そう</b>　<input type='text' name='url' style='width:600px'>　
 	<b>自宅</b> <input type='text' name='home'> 
-	&nbsp;&nbsp;&nbsp;<input type='submit' value='探す'>
-	&nbsp;&nbsp;&nbsp;<a href='explain' target='_blank' style='text-decoration: none; font-size: 12px;'>使い方</a>
+	<input type='submit' value='探す'>
+	<a href='explain' target='_blank' style='text-decoration: none; font-size: 12px;'>使い方</a>
 </form>
 
 
